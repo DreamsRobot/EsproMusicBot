5b7082931da53fc26a3f198414cc1ab517a93586
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-08-14 17:13:00 +0530

    Update start.py

--- EsproMusic/utils/inline/start.py
+++ EsproMusic/utils/inline/start.py
@@ -22,17 +22,13 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["D_S_1"], callback_data=""),
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
     ]
     return buttons
